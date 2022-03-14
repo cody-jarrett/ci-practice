b780af1e6400a1fde5f4b9b50c0bfe21c1f6854d
cody-jarrett <cody-jarrett@users.noreply.github.com>
2022-03-14 15:00:12 -0700

    add add, subtract, tests

--- ci_practice/main.py
+++ ci_practice/main.py
@@ -3,5 +3,13 @@ import toml
 CONFIG = toml.load("config.toml")["app"]
 
 
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
 def app():
     print(f"This is {CONFIG['APP_NAME']}. Let's build some cool python apps!")
--- tests/test_ci_practice.py
+++ tests/test_ci_practice.py
@@ -1,5 +1,13 @@
-from ci_practice import __version__
+from ci_practice import __version__, main
 
 
 def test_version():
     assert __version__ == "0.2.0"
+
+
+def test_addition():
+    assert 4 == main.add(2, 2)
+
+
+def test_subtraction():
+    assert 2 == main.subtract(4, 2)
